f5fea3ad11b7ae0811f1fa5d2af31ba29c8fca03
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-11-27 23:26:34 +0900

    콤포즈 코드랩 week 1 완료

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -16,27 +16,22 @@
 
 package com.codelab.basics
 
+import android.content.res.Configuration
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
@@ -48,36 +43,143 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.BasicsCodelabTheme
+import com.codelab.basics.ui.Green200
+import com.codelab.basics.ui.Red200
+import com.codelab.basics.ui.Teal200
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                MyApp()
+                // A surface container using the 'background' color from the theme
+                ContentView()
+
             }
         }
     }
 }
 
 @Composable
-private fun MyApp() {
+private fun ContentView(){
+
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        OnboardingScreen(onContinueClicked = {
+            Log.d("TAG", "ContentView: 온보딩 클릭됨")
+            shouldShowOnboarding = !shouldShowOnboarding
+        })
     } else {
-        Greetings()
+//        MyColumn(modifier = Modifier.padding(20.dp))
+        MyLazyColumn(modifier = Modifier.padding(20.dp))
+    }
+
+//    Surface(color = Green200) {
+////        HelloWorld(modifier = Modifier.padding(all = 10.dp))
+//        MyColumn(modifier = Modifier.padding(10.dp))
+//    }
+
+}
+
+@Composable
+fun MyColumn(fruits: List<String> = listOf("바나나", "딸기", "포도"), modifier: Modifier) {
+    Column (modifier = modifier) {
+        for (aFruitName in fruits) {
+            FruitView(aFruitName)
+        }
     }
 }
 
 @Composable
-private fun OnboardingScreen(onContinueClicked: () -> Unit) {
+fun MyLazyColumn(fruits: List<String> = List(1000) { "번호 : $it" } , modifier: Modifier) {
+    LazyColumn (modifier = modifier) {
+        items(items = fruits) { aFruit ->
+            FruitView(name = aFruit)
+        }
+    }
+}
+
+
+//- `val mutableState = remember { mutableStateOf(default) }`
+//- `var value by remember { mutableStateOf(default) }`
+//- `val (value, setValue) = remember { mutableStateOf(default) }`
+
+@Composable
+fun FruitView(name: String){
+
+    val expanded = rememberSaveable { mutableStateOf(false) }
+
+    var isOpen by remember { mutableStateOf(false) }
+
+    val (shouldOpen, setShouldOpen) = remember { mutableStateOf(false) }
+
+//    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+    )
+
+//    val extraPadding = animateDpAsState(
+//        if (expanded.value) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
+
+    Surface(color = Red200,
+        modifier = Modifier
+            .padding(bottom = extraPadding)
+            .clickable { Log.d("TAG", "FruitView: $name") })
+    {
+        Row(verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(end = 10.dp)
+                .fillMaxWidth(fraction = 1.0f)
+        ) {
+            Text(text = name,
+                modifier = Modifier
+//                    .fillMaxWidth(fraction = 1.0f)
+                    .padding(10.dp)
+                    .background(Teal200)
+                    .weight(0.5f),
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            OutlinedButton(
+//                modifier = Modifier.weight(0.5f) ,
+                onClick = {
+                    expanded.value = !expanded.value
+//                    isOpen = !isOpen
+//                    setShouldOpen(!shouldOpen)
+                    Log.d("TAG", "FruitView: 버튼 클릭 $name")
+                }) {
+                Text(text = if (expanded.value) "열렸다" else "닫혔다")
+//                Text(text = if (isOpen) "열렸다" else "닫혔다")
+//                Text(text = if (shouldOpen) "열렸다" else "닫혔다")
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -96,89 +198,34 @@ private fun OnboardingScreen(onContinueClicked: () -> Unit) {
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name)
-        }
-    }
+fun Greeting(name: String) {
+    Text(text = "오늘도 $name!")
 }
 
-@Composable
-private fun Greeting(name: String) {
-    Card(
-        backgroundColor = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        CardContent(name)
-    }
-}
+//@Composable
+//private fun Greeting(name: String) {
+//    Surface(color = MaterialTheme.colors.primary) {
+//        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+//    }
+//}
 
 @Composable
-private fun CardContent(name: String) {
-    var expanded by remember { mutableStateOf(false) }
-
-    Row(
-        modifier = Modifier
-            .padding(12.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                    stiffness = Spring.StiffnessLow
-                )
-            )
-    ) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(12.dp)
-        ) {
-            Text(text = "Hello, ")
-            Text(
-                text = name,
-                style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                )
-            )
-            if (expanded) {
-                Text(
-                    text = ("Composem ipsum color sit lazy, " +
-                        "padding theme elit, sed do bouncy. ").repeat(4),
-                )
-            }
-        }
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(
-                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
-                contentDescription = if (expanded) {
-                    stringResource(R.string.show_less)
-                } else {
-                    stringResource(R.string.show_more)
-                }
-
-            )
-        }
+fun HelloWorld(modifier: Modifier) {
+    Surface(color = Green200, modifier = modifier) {
+        Text(text = "HELLO WORLD", color = Color.White, modifier = Modifier.padding(all = 30.dp))
     }
 }
 
 @Preview(
     showBackground = true,
     widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
     name = "DefaultPreviewDark"
 )
-@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        Greetings()
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    BasicsCodelabTheme {
-        OnboardingScreen(onContinueClicked = {})
+//        ContentView()
+        MyLazyColumn(modifier = Modifier.padding(20.dp))
     }
 }
--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/Color.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/Color.kt
@@ -18,7 +18,11 @@ package com.codelab.basics.ui
 
 import androidx.compose.ui.graphics.Color
 
-val Navy = Color(0xFF073042)
-val Blue = Color(0xFF4285F4)
-val LightBlue = Color(0xFFD7EFFE)
-val Chartreuse = Color(0xFFEFF7CF)
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+
+val Red200 = Color(0xFFF48FB1)
+val Green200 = Color(0xFFA5D6A7)
+val Teal200 = Color(0xFF03DAC5)
